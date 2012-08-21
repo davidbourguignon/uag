53dadf74c3daa5409ce85d6ce9eb4991e3987e27
SebViv <svivierpro@gmail.com>
2012-08-21 12:31:21 +0200

    DAL test forward

--- FieldTests/WebContent/js/spec/dataModelSpec.js
+++ FieldTests/WebContent/js/spec/dataModelSpec.js
@@ -8,25 +8,36 @@
 //console.log(uag.data.model.position.prototype.toSource());
 //console.log(p1.toSource());
  
-describe("position", function() {
+describe("Position", function() {
 
 	// 4 arguments constructor 
 	it("should be able to initialize position object with 4 arguments", function() {
+		
 		var id1 = "position_id_1"; 
 		var lat1 = 2; 
 		var lon1 = 4;
 		var alt1 = 8;
-		var position = new uag.data.model.position(id1, lat1, lon1, alt1);
+		
+		var position = new uag.data.model.Position(id1, lat1, lon1, alt1);
+		
 		expect(position.getId()).toEqual(id1);
 		expect(position.getLatitude()).toEqual(lat1);
 		expect(position.getLongitude()).toEqual(lon1);
 		expect(position.getAltitude()).toEqual(alt1);
+		
+		jsonPosition = JSON.parse(position.toString());
+		
+		expect(position.getId()).toEqual(jsonPosition.id);
+		expect(position.getLatitude()).toEqual(+jsonPosition.latitude);
+		expect(position.getLongitude()).toEqual(+jsonPosition.longitude);
+		expect(position.getAltitude()).toEqual(+jsonPosition.altitude);
+		
 	});
 	
 	// 1 arguments constructor 
 	it("should be able to initialize position object with 1 json argument", function() {
-		var jsonData = { "id":"position_id_2","latitude":1,"longitude":2,"altitude":4 }
-		var position = new uag.data.model.position( jsonData );
+		var jsonData = { "id":"position_id_2","latitude":1,"longitude":2,"altitude":4 };
+		var position = new uag.data.model.Position( jsonData );
 		expect(position.getId()).toEqual(jsonData.id);
 		expect(position.getLatitude()).toEqual(jsonData.latitude);
 		expect(position.getLongitude()).toEqual(jsonData.longitude);
@@ -36,37 +47,37 @@ describe("position", function() {
 	// other constructors based on arguments bad number (0, 2, 3, 5 arguments) or type (1+ null arguments, ...) 
 	it("should throw exception if bad arguments", function() {
 		expect(function(){
-			var position = new uag.data.model.position();
+			var position = new uag.data.model.Position();
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 		expect(function(){
-			var position = new uag.data.model.position("", 10);
+			var position = new uag.data.model.Position("", 10);
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 		expect(function(){
-			var position = new uag.data.model.position("", 10, 20);
+			var position = new uag.data.model.Position("", 10, 20);
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 		expect(function(){
-			var position = new uag.data.model.position("", 10, 20, 30, 40);
+			var position = new uag.data.model.Position("", 10, 20, 30, 40);
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 		expect(function(){
-			var position = new uag.data.model.position("", 10, 20, null);
+			var position = new uag.data.model.Position("", 10, 20, null);
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 	});
 	
 	// others constructor based on bad json input data 
 	it("should throw exception if bad json arguments", function() {
 		expect(function(){
-			var jsonData = { "id":null,"latitude":1,"longitude":2,"altitude":4 }
-			var position = new uag.data.model.position( jsonData );
+			var jsonData = { "id":null,"latitude":1,"longitude":2,"altitude":4 };
+			var position = new uag.data.model.Position( jsonData );
 			alert(position.getId());
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 		expect(function(){
-			var jsonData = { "latitude":1,"longitude":2,"altitude":4 }
-			var position = new uag.data.model.position( jsonData );
+			var jsonData = { "latitude":1,"longitude":2,"altitude":4 };
+			var position = new uag.data.model.Position( jsonData );
 			alert(position.getId());
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 		expect(function(){
-			var jsonData = { "identifier":"position_id_2","latitude":1,"longitude":2,"altitude":4 }
-			var position = new uag.data.model.position( jsonData );
+			var jsonData = { "identifier":"position_id_2","latitude":1,"longitude":2,"altitude":4 };
+			var position = new uag.data.model.Position( jsonData );
 			alert(position.getId());
 		}).toThrow("bad arguments exception : position expected 1 or 4 arguments !!");
 	});
@@ -74,7 +85,7 @@ describe("position", function() {
 });
 
 
-describe("field", function() {
+describe("Field", function() {
 
 	// 6 arguments constructor
 	it("should be able to initialize field object with 6 arguments", function() {
@@ -84,13 +95,25 @@ describe("field", function() {
 		var positionId = "position_id_1"; 
 		var productsId = ["product_id_1","product_id_2","product_id_3"]; 
 		var producerId = "procucer_id_1"; 
-		var field = new uag.data.model.field(id, name, description, positionId, productsId, producerId);
+		
+		var field = new uag.data.model.Field(id, name, description, positionId, productsId, producerId);
+		
 		expect(field.getId()).toEqual(id);
 		expect(field.getName()).toEqual(name);
 		expect(field.getDescription()).toEqual(description);
 		expect(field.getPositionId()).toEqual(positionId);
 		expect(field.getProductsId()).toEqual(productsId);
 		expect(field.getProducerId()).toEqual(producerId);
+		
+		jsonField = JSON.parse(field.toString());
+		
+		expect(field.getId()).toEqual(jsonField.id);
+		expect(field.getName()).toEqual(jsonField.name);
+		expect(field.getDescription()).toEqual(jsonField.description);
+		expect(field.getPositionId()).toEqual(jsonField.positionId);
+		expect(field.getProductsId()).toEqual(jsonField.productsId);
+		expect(field.getProducerId()).toEqual(jsonField.producerId);
+
 	});
 	
 	// 1 json arguments constructor
@@ -103,7 +126,7 @@ describe("field", function() {
 			"productsId":["product_id_1","product_id_2","product_id_3"], 
 			"producerId":"procucer_id_1" 
 		};
-		var field = new uag.data.model.field(jsonData);
+		var field = new uag.data.model.Field(jsonData);
 		expect(field.getId()).toEqual(jsonData.id);
 		expect(field.getName()).toEqual(jsonData.name);
 		expect(field.getDescription()).toEqual(jsonData.description);
@@ -115,10 +138,10 @@ describe("field", function() {
 });
 
 
-describe("producer", function() {
+describe("Producer", function() {
 
 	// 7 arguments constructor
-	it("should be able to initialize producer object with 7 arguments", function() {
+	it("should be able to initialize producer object with 8 arguments", function() {
 		var id = "producer_id_1";
 		var name = "nom";
 		var smartphoneId = "smart_phone_id";
@@ -126,16 +149,18 @@ describe("producer", function() {
 		var websiteUrl = "http://www.domaine.ext";
 		var workersId = ["worker_id_1","worker_id_2","worker_id_3"];
 		var fieldsId = ["field_id_1","field_id_2","field_id_3"];
+		var productsId = ["product_id_1","product_id_2","product_id_3"];
 		
 		var producer = 
-			new uag.data.model.producer(
+			new uag.data.model.Producer(
 				id, 
 				name, 
 				smartphoneId, 
 				farmName, 
 				websiteUrl, 
 				workersId, 
-				fieldsId);
+				fieldsId,
+				productsId);
 		
 		expect(producer.getId()).toEqual(id);
 		expect(producer.getName()).toEqual(name);
@@ -144,6 +169,19 @@ describe("producer", function() {
 		expect(producer.getWebsiteUrl()).toEqual(websiteUrl);
 		expect(producer.getWorkersId()).toEqual(workersId);
 		expect(producer.getFieldsId()).toEqual(fieldsId);
+		expect(producer.getProductsId()).toEqual(productsId);
+		
+		jsonProducer = JSON.parse(producer.toString());
+		
+		expect(producer.getId()).toEqual(jsonProducer.id);
+		expect(producer.getName()).toEqual(jsonProducer.name);
+		expect(producer.getSmartphoneId()).toEqual(jsonProducer.smartphoneId);
+		expect(producer.getFarmName()).toEqual(jsonProducer.farmName);
+		expect(producer.getWebsiteUrl()).toEqual(jsonProducer.websiteUrl);
+		expect(producer.getWorkersId()).toEqual(jsonProducer.workersId);
+		expect(producer.getFieldsId()).toEqual(jsonProducer.fieldsId);
+		expect(producer.getProductsId()).toEqual(jsonProducer.productsId);
+		
 	});
 	
 	// 1 json arguments constructor
@@ -156,9 +194,10 @@ describe("producer", function() {
 			"websiteUrl":"http://www.domaine.ext", 
 			"workersId":["worker_id_1","worker_id_2","worker_id_3"], 
 			"fieldsId":["field_id_1","field_id_2","field_id_3"], 
+			"productsId":["product_id_1","product_id_2","product_id_3"]
 		};
 		
-		var producer = new uag.data.model.producer(jsonData);
+		var producer = new uag.data.model.Producer(jsonData);
 		
 		expect(producer.getId()).toEqual(jsonData.id);
 		expect(producer.getName()).toEqual(jsonData.name);
@@ -167,6 +206,8 @@ describe("producer", function() {
 		expect(producer.getWebsiteUrl()).toEqual(jsonData.websiteUrl);
 		expect(producer.getWorkersId()).toEqual(jsonData.workersId);
 		expect(producer.getFieldsId()).toEqual(jsonData.fieldsId);
+		expect(producer.getProductsId()).toEqual(jsonData.productsId);
+		
 	});
 
 });
@@ -183,7 +224,7 @@ describe("worker", function() {
 		var producersId = ["producer_id_1","producer_id_2","producer_id_3"];
 		
 		var worker = 
-			new uag.data.model.worker(
+			new uag.data.model.Worker(
 				id, 
 				name, 
 				smartphoneId, 
@@ -207,7 +248,7 @@ describe("worker", function() {
 			"producersId":["producer_id_1","producer_id_2","producer_id_3"]
 		};
 		
-		var worker = new uag.data.model.worker(jsonData);
+		var worker = new uag.data.model.Worker(jsonData);
 		
 		expect(worker.getId()).toEqual(jsonData.id);
 		expect(worker.getName()).toEqual(jsonData.name);
@@ -219,16 +260,157 @@ describe("worker", function() {
 });
 
 
-describe("product", function() {
+describe("Product", function() {
+
+	// 3 arguments constructor
+	it("should be able to initialize Product object with 3 arguments", function() {
+		var id = "product_id_1"; 
+		var name = "productName"; 
+		var description = "productDescription"; 
+		
+		var product = 
+			new uag.data.model.Product(id, name, description);
+		
+		expect(product.getId()).toEqual(id);
+		expect(product.getName()).toEqual(name);
+		expect(product.getDescription()).toEqual(description);
+		
+		jsonProduct = JSON.parse(product.toString());
+		
+		expect(product.getId()).toEqual(jsonProduct.id);
+		expect(product.getName()).toEqual(jsonProduct.name);
+		expect(product.getDescription()).toEqual(jsonProduct.description);
+
+	});
+	
+	// 1 json arguments constructor
+	it("should be able to initialize Product object with 1 json arguments", function() {
+		var jsonData = {
+			"id":"product_id_1", 
+			"name":"productName",
+			"description":"productDescription"};
+		
+		var product = new uag.data.model.Product(jsonData);
+		
+		expect(product.getId()).toEqual(jsonData.id);
+		expect(product.getName()).toEqual(jsonData.name);
+		expect(product.getDescription()).toEqual(jsonData.description);
+		
+	});
+	
+});
+
+
+describe("Container", function() {
+
+	// 8 arguments constructor
+	it("should be able to initialize Container object with 8 arguments", function() {
+		var id = "container_id_1";
+		var weight = 1.5;
+		var ts = Date.now();
+		var qualityId = "quality_id_1";
+		var workerId = "worker_id_1";
+		var producerId = "producer_id_1";
+		var fieldId = "field_id_1";
+		var productId = "product_id_1";
+		
+		var container = 
+			new uag.data.model.Container(
+					id, 
+					weight, 
+					ts,
+					qualityId,
+					workerId,
+					producerId,
+					fieldId,
+					productId);
+		
+		expect(container.getId()).toEqual(id);
+		expect(container.getWeight()).toEqual(weight);
+		expect(container.getTs()).toEqual(ts);
+		expect(container.getQualityId()).toEqual(qualityId);
+		expect(container.getWorkerId()).toEqual(workerId);
+		expect(container.getProducerId()).toEqual(producerId);
+		expect(container.getFieldId()).toEqual(fieldId);
+		expect(container.getProductId()).toEqual(productId);
+		
+		jsonContainer = JSON.parse(container.toString());
+		
+		expect(container.getId()).toEqual(jsonContainer.id);
+		expect(container.getWeight()).toEqual(jsonContainer.weight);
+		expect(container.getTs()).toEqual(jsonContainer.ts);
+		expect(container.getQualityId()).toEqual(jsonContainer.qualityId);
+		expect(container.getWorkerId()).toEqual(jsonContainer.workerId);
+		expect(container.getProducerId()).toEqual(jsonContainer.producerId);
+		expect(container.getFieldId()).toEqual(jsonContainer.fieldId);
+		expect(container.getProductId()).toEqual(jsonContainer.productId);
+
+	});
+	
+	// 1 json arguments constructor
+	it("should be able to initialize Container object with 1 json arguments", function() {
+		var jsonData = {
+			"id":"container_id_1",
+			"weight":1.5,
+			"ts":Date.now(),
+			"qualityId":"quality_id_1",
+			"workerId":"worker_id_1",
+			"producerId":"producer_id_1",
+			"fieldId":"field_id_1",
+			"productId":"product_id_1"};
+		
+		var container = new uag.data.model.Container(jsonData);
+		
+		expect(container.getId()).toEqual(jsonData.id);
+		expect(container.getWeight()).toEqual(jsonData.weight);
+		expect(container.getTs()).toEqual(jsonData.ts);
+		expect(container.getQualityId()).toEqual(jsonData.qualityId);
+		expect(container.getWorkerId()).toEqual(jsonData.workerId);
+		expect(container.getProducerId()).toEqual(jsonData.producerId);
+		expect(container.getFieldId()).toEqual(jsonData.fieldId);
+		expect(container.getProductId()).toEqual(jsonData.productId);
+		
+	});
 
 });
 
 
-describe("container", function() {
+describe("Quality", function() {
 
-});
+	// 3 arguments constructor
+	it("should be able to initialize Product object with 3 arguments", function() {
+		var id = "quality_id_1"; 
+		var name = "qualityName"; 
+		var description = "qualityDescription"; 
+		
+		var quality = 
+			new uag.data.model.Quality(id, name, description);
+		
+		expect(quality.getId()).toEqual(id);
+		expect(quality.getName()).toEqual(name);
+		expect(quality.getDescription()).toEqual(description);
+		
+		jsonQuality = JSON.parse(quality.toString());
+		
+		expect(quality.getId()).toEqual(jsonQuality.id);
+		expect(quality.getName()).toEqual(jsonQuality.name);
+		expect(quality.getDescription()).toEqual(jsonQuality.description);
 
-
-describe("quality", function() {
+	});
+	
+	// 1 json arguments constructor
+	it("should be able to initialize Product object with 1 json arguments", function() {
+		var jsonData = {
+			"id":"quality_id_1", 
+			"name":"qualityName",
+			"description":"qualityDescription"};
+		
+		var quality = new uag.data.model.Quality(jsonData);
+		
+		expect(quality.getId()).toEqual(jsonData.id);
+		expect(quality.getName()).toEqual(jsonData.name);
+		expect(quality.getDescription()).toEqual(jsonData.description);
+		
+	});
 
 });
--- FieldTests/WebContent/js/spec/dataSpec.js
+++ FieldTests/WebContent/js/spec/dataSpec.js
@@ -2,53 +2,226 @@
  * @author Sébastien Vivier, NSY209, CNAM 
  * @version Copyright (c) 2012
  * */
+
+
+describe("Workers", function() {
 	
- 
-describe("workers", function() {
-	
-	it("Should ... ", function() {
-				
+	it("Should do all Workers local storage basic actions", function() {
+			
 		var workers = null;
 		var workerName = "workerName";
-		var workerGetAll = null;
-		var workerFindById = null;
+		var firstGetAllWorker = null;
+		var findByIdWorker = null;
 		
 		// unset local storage
-		uag.data.workers.clear();
+		uag.data.Workers.clear();
 		
 		// fetch data 
-		workers = uag.data.workers.getAll();
+		workers = uag.data.Workers.getAll();
 		
-		expect(Array.isArray(workers)).toEqual(true);
+		expect(uag.util.Type.isArray(workers)).toEqual(true);
 		expect(workers.length).toEqual(0);
 		
 		// add worker 
-		uag.data.workers.add(workerName);	
-		
-		//alert(localStorage.getItem("workers"));
+		uag.data.Workers.add(workerName);	
 
 		// fetch all data
-		workers = uag.data.workers.getAll();
+		workers = uag.data.Workers.getAll();
 		
-		expect(Array.isArray(workers)).toEqual(true);
+		expect(uag.util.Type.isArray(workers)).toEqual(true);
 		expect(workers.length).toEqual(1);
 		
-		//alert(workers[0].getId());
-		workerGetAll = workers[0];
-		expect(workerGetAll.getName()).toEqual(workerName);
-		
+		firstGetAllWorker = workers[0];
+		expect(firstGetAllWorker).not.toEqual(undefined);
+		expect(firstGetAllWorker).not.toEqual(null);
+		expect(firstGetAllWorker.getName()).toEqual(workerName);
 		
 		// find worker by id
-		workerFindById = uag.data.workers.findById(workerGetAll.getId());
+		findByIdWorker = uag.data.Workers.findById(firstGetAllWorker.getId());
 		
+		expect(firstGetAllWorker.getId()).toEqual(findByIdWorker.getId());
+		expect(firstGetAllWorker.getName()).toEqual(findByIdWorker.getName());
+		expect(firstGetAllWorker.getSmartphoneId()).toEqual(findByIdWorker.getSmartphoneId());
 		
-		expect(workerGetAll.getId()).toEqual(workerFindById.getId());
-		expect(workerGetAll.getName()).toEqual(workerFindById.getName());
-		expect(workerGetAll.getSmartphoneId()).toEqual(workerFindById.getSmartphoneId());
+		// do not forget remove tests
+		var workerId = firstGetAllWorker.getId();
+		uag.data.Workers.remove(workerId);
 		
-		//expect(workerGetAll).toEqual(workerFindById);
+		// fetch data 
+		workers = uag.data.Workers.getAll();
 		
-		//expect(workerGetAll.).toEqual(workerFindById.);
+		expect(uag.util.Type.isArray(workers)).toEqual(true);
+		expect(workers.length).toEqual(0);
+		
+	});
+	
+});
+
+
+describe("Producers",function(){
+	
+	
+	it("should do all Producers local storage basic actions", function() {
+
+		// unset local storage
+		uag.data.Producers.clear();
+		
+		// fetch data 
+		producers = uag.data.Producers.getAll(null);
+		
+		expect(uag.util.Type.isArray(producers)).toEqual(true);
+		expect(producers.length).toEqual(0);
+
+		
+	});
+	
+});
+
+
+describe("Import", function() {
+	
+	var _jsonDataString1 = 
+		'{' + 
+			'"id":"producer_id_1",' +  
+			'"name":"producerName",' + 
+			'"smartphoneId":"producerSmartphoneId",' +  
+			'"farmName":"producerFarmname",' + 
+			'"websiteUrl":"http://www.producerDomaine.ext",' +  
+			'"workersId":[],' + 
+			'"fieldsId":' + 
+			'[' + 
+				'"field_id_1",' + 
+				'"field_id_2",' + 
+				'"field_id_3"' + 
+			'],' + 
+			'"fields":' + 
+			'[' + 
+				'{' + 
+					'"id":"field_id_1",' + 
+					'"name":"fieldName1",' + 
+					'"description":"fieldDescription1",' +  
+					'"producerId":"producer_id_1",' + 
+					'"positionId":"position_id_1",' +  
+					'"position":{"id":"position_id_1","latitude":1,"longitude":2,"altitude":4},' +
+					'"productsId":' + 
+					'[' + 
+						'"product_id_1",' + 
+						'"product_id_2",' + 
+						'"product_id_3"' + 
+					']' + 
+				'},' +  
+				'{' + 
+					'"id":"field_id_2",' + 
+					'"name":"fieldName2",' + 
+					'"description":"fieldDescription2",' +  
+					'"producerId":"producer_id_1",' + 
+					'"positionId":"position_id_2",' +   
+					'"position":{"id":"position_id_2","latitude":2,"longitude":4,"altitude":8},' +
+					'"productsId":' + 
+					'[' + 
+						'"product_id_2",' + 
+						'"product_id_3"' + 
+					']' + 
+				'},' +  
+				'{' + 
+					'"id":"field_id_3",' + 
+					'"name":"fieldName3",' + 
+					'"description":"fieldDescription3",' +  
+					'"producerId":"producer_id_1",' + 
+					'"positionId":"position_id_3",' +   
+					'"position":{"id":"position_id_3","latitude":3,"longitude":6,"altitude":12},' +
+					'"productsId":' + 
+					'[' + 
+						'"product_id_3",' + 
+						'"product_id_4",' + 
+						'"product_id_5",' + 
+						'"product_id_6"' + 
+					']' + 
+				'}' +  
+			'],' + 
+			'"productsId":' + 
+			'[' + 
+				'"product_id_1",' + 
+				'"product_id_2",' + 
+				'"product_id_3",' + 
+				'"product_id_4",' + 
+				'"product_id_5",' + 
+				'"product_id_6"' + 
+			'],' + 
+			'"products":' + 
+			'[' + 
+				'{' +
+					'"id":"product_id_1",' + 
+					'"name":"productName1",' + 
+					'"description":"productDescription1"' +  
+				'},' + 
+				'{' +
+					'"id":"product_id_2",' + 
+					'"name":"productName2",' + 
+					'"description":"productDescription2"' +  
+				'},' + 
+				'{' +
+					'"id":"product_id_3",' + 
+					'"name":"productName3",' + 
+					'"description":"productDescription3"' +  
+				'},' + 
+				'{' +
+					'"id":"product_id_4",' + 
+					'"name":"productName4",' + 
+					'"description":"productDescription4"' +  
+				'},' + 
+				'{' +
+					'"id":"product_id_5",' + 
+					'"name":"productName5",' + 
+					'"description":"productDescription5"' +  
+				'},' + 
+				'{' +
+					'"id":"product_id_6",' + 
+					'"name":"productName6",' + 
+					'"description":"productDescription6"' +  
+				'}' + 
+			']' + 
+		'}';
+
+
+	
+	it("Should import data to local storage", function() {
+		
+		uag.data.Producers.clear();
+		
+		//var key = uag.data.Producers.getLocalStorageKey();		
+		uag.data.Import.set(_jsonDataString1);
+		//var producers = localStorage.getItem(key);
+		//alert(producers);
+		
+		var jsonData = JSON.parse(_jsonDataString1);
+		
+		var producer = new uag.data.model.Producer(jsonData);
+		
+		var products = new Array();
+		for ( var int = 0 ; int < jsonData.products.length ; int++ ) {
+			products.push(
+					new uag.data.model.Product(jsonData.products[int]));
+		}
+		producer.products = products;
+		
+		var fields = new Array();
+		for ( var int = 0 ; int < jsonData.fields.length ; int++ ) {
+			var field = new uag.data.model.Field(jsonData.fields[int]);
+			var position = new uag.data.model.Position(jsonData.fields[int].position);
+			field.position = position;
+			
+			for (var i = 0 ; i < products.length ; i++) {
+				field.products = new Array();
+				if (-1 !== field.getProductsId().indexOf(products[i].getId())) {
+					field.products.push(products[i]);
+				}
+			}
+			
+			fields.push(field);
+		}
+		producer.fields = fields;
+		
 		
 		
 	});
