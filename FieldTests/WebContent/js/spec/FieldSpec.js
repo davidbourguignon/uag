1bcabfe452dcf98e0ae8766b9c785ce9c6a506ae
SebViv <svivierpro@gmail.com>
2012-08-27 20:01:38 +0200

    minor changes

--- FieldTests/WebContent/js/spec/FieldSpec.js
+++ FieldTests/WebContent/js/spec/FieldSpec.js
@@ -1,4 +1,4 @@
-/**@fileOverview uag data specification logic 
+/**@fileOverview uag Field app dataContext specification logic 
  * @author Sébastien Vivier, NSY209, CNAM 
  * @version Copyright (c) 2012
  * */
@@ -13,11 +13,11 @@ describe("Namespace like basics",function(){
 		expect(uag.util).not.toEqual(null || undefined);
 		expect(uag.util.Storage).not.toEqual(null || undefined);
 		
-		expect(uag.data).not.toEqual(null || undefined);
-		expect(uag.data.Producers).not.toEqual(null || undefined);
-		expect(uag.data.WorkerContainers).not.toEqual(null || undefined);
-		expect(uag.data.Import).not.toEqual(null || undefined);
-		expect(uag.data.Export).not.toEqual(null || undefined);
+		expect(uag.field.dataContext).not.toEqual(null || undefined);
+		expect(uag.field.dataContext.Producers).not.toEqual(null || undefined);
+		expect(uag.field.dataContext.WorkerContainers).not.toEqual(null || undefined);
+		expect(uag.field.dataContext.Import).not.toEqual(null || undefined);
+		expect(uag.field.dataContext.Export).not.toEqual(null || undefined);
 		
 		expect(function() {
 			Storage.Retrive("");
@@ -39,8 +39,8 @@ describe("Namespace like basics",function(){
 			Export.get();
 		}).toThrow();
 		
-		//uag.util.Storage.Delete(uag.data.Producers.getLocalStorageKey());
-		//uag.util.Storage.Delete(uag.data.WorkerContainers.getLocalStorageKey());
+		//uag.util.Storage.Delete(uag.field.dataContext.Producers.getLocalStorageKey());
+		//uag.util.Storage.Delete(uag.field.dataContext.WorkerContainers.getLocalStorageKey());
 		//uag.util.Storage.Delete("setup");
 		//uag.util.Storage.Delete("workers");
 		
@@ -53,11 +53,11 @@ describe("Producers basic",function(){
 	
 	it("should do all Producers local storage basic actions", function() {
 		
-		uag.data.Producers.clear();
+		uag.field.dataContext.Producers.clear();
 		
 		var producers = null;
 		expect(function() {
-			producers = uag.data.Producers.get();
+			producers = uag.field.dataContext.Producers.get();
 		}).not.toThrow();
 		
 		expect(producers).not.toEqual(null || undefined);
@@ -66,14 +66,14 @@ describe("Producers basic",function(){
 		
 		var producer = null;
 		expect(function() {
-			producer = uag.data.Producers.find();
+			producer = uag.field.dataContext.Producers.find();
 		}).not.toThrow();
 
 		expect(producer).toEqual(null);
 		
 		var fields = null;
 		expect(function() {
-			fields = uag.data.Producers.getFields();
+			fields = uag.field.dataContext.Producers.getFields();
 		}).not.toThrow();
 		
 		expect(fields).toEqual([]);
@@ -82,21 +82,21 @@ describe("Producers basic",function(){
 		
 		var field = null;
 		expect(function() {
-			field = uag.data.Producers.findField(fieldId);
+			field = uag.field.dataContext.Producers.findField(fieldId);
 		}).not.toThrow();
 		
 		expect(field).toEqual(null);
 		
 		var products = null;
 		expect(function() {
-			products = uag.data.Producers.getProducts();
+			products = uag.field.dataContext.Producers.getProducts();
 		}).not.toThrow();
 		
 		expect(products).toEqual([]);
 		
 		var fieldProducts = null;
 		expect(function() {
-			fieldProducts = uag.data.Producers.getFieldProducts(fieldId);
+			fieldProducts = uag.field.dataContext.Producers.getFieldProducts(fieldId);
 		}).not.toThrow();
 		
 		expect(fieldProducts).toEqual([]);
@@ -105,14 +105,14 @@ describe("Producers basic",function(){
 		
 		var fieldProduct = null;
 		expect(function() {
-			fieldProduct = uag.data.Producers.findFieldProduct(fieldId, productId);
+			fieldProduct = uag.field.dataContext.Producers.findFieldProduct(fieldId, productId);
 		}).not.toThrow();
 		
 		expect(fieldProduct).toEqual(null);
 		
 		var qualities = null;
 		expect(function() {
-			qualities = uag.data.Producers.getQualities();
+			qualities = uag.field.dataContext.Producers.getQualities();
 		}).not.toThrow();
 		
 		expect(qualities).toEqual([]);
@@ -126,18 +126,18 @@ describe("workerContainers basic", function() {
 	
 	it("Should do all WorkerContainers local storage basic actions", function() {
 		
-		uag.data.WorkerContainers.clear();
+		uag.field.dataContext.WorkerContainers.clear();
 		
 		var worker = null;
 		expect(function() {
-			worker = uag.data.WorkerContainers.getWorker();
+			worker = uag.field.dataContext.WorkerContainers.getWorker();
 		}).not.toThrow();
 		
 		expect(worker.isAvailable).toEqual(false);
 		
 		var containers = null;
 		expect(function() {
-			containers = uag.data.WorkerContainers.getContainers();
+			containers = uag.field.dataContext.WorkerContainers.getContainers();
 		}).not.toThrow();
 		
 		expect(containers).not.toEqual(null);
@@ -145,7 +145,7 @@ describe("workerContainers basic", function() {
 
 		var container = null;
 		expect(function() {
-			container = uag.data.WorkerContainers.findContainer("container_id_fake");
+			container = uag.field.dataContext.WorkerContainers.findContainer("container_id_fake");
 		}).not.toThrow();
 
 		expect(container).toEqual(null);
@@ -154,32 +154,32 @@ describe("workerContainers basic", function() {
 	
 	it("Should do WorkerContainers add container action ",function() {
 		
-		var workerId = uag.data.WorkerContainers.setWorker("fakeWorker");
+		var workerId = uag.field.dataContext.WorkerContainers.setWorker("fakeWorker");
 		
-		worker = uag.data.WorkerContainers.getWorker();
+		worker = uag.field.dataContext.WorkerContainers.getWorker();
 		
 		expect(worker.isAvailable).toEqual(true);
 		expect(worker.id).toEqual(workerId);
 		
 		var containerId = 
-			uag.data.WorkerContainers.addContainer(
+			uag.field.dataContext.WorkerContainers.addContainer(
 					2.1, 
 					"quality_id_fake", 
 					"field_id_fake", 
 					"product_id_fake");
 		
-		var containers = uag.data.WorkerContainers.getContainers();
+		var containers = uag.field.dataContext.WorkerContainers.getContainers();
 		
 		expect(containers.length).toEqual(1);
 		
-		var container = uag.data.WorkerContainers.findContainer(containerId);
+		var container = uag.field.dataContext.WorkerContainers.findContainer(containerId);
 
 		expect(container.id).toEqual(containerId);
 		expect(container.id).toEqual(containers[0].id);
 		
-		uag.data.WorkerContainers.removeContainer(containerId);
+		uag.field.dataContext.WorkerContainers.removeContainer(containerId);
 		
-		containers = uag.data.WorkerContainers.getContainers();
+		containers = uag.field.dataContext.WorkerContainers.getContainers();
 		
 		expect(containers).not.toEqual(null);
 		expect(containers.length).toEqual(0);		
@@ -193,9 +193,9 @@ describe("Import", function() {
 	
 	it("Should import producer data to local storage, only one", function() {
 		
-		uag.data.Producers.clear();
+		uag.field.dataContext.Producers.clear();
 		
-		var producers = uag.data.Producers.get();
+		var producers = uag.field.dataContext.Producers.get();
 		
 		expect(producers).not.toEqual(null);
 		expect(producers).toEqual([]);
@@ -204,12 +204,12 @@ describe("Import", function() {
 		var initialProducerNumber = producers.length;
 		
 		// add producer to producers list (for the first time) 
-		var producerId = uag.data.Import.addProducer(_jsonProducer1Data);
+		var producerId = uag.field.dataContext.Import.addProducer(_jsonProducer1Data);
 		
 		expect(producerId).toEqual(_producerId1);
 		expect(producerId).toEqual(_jsonProducer1Data.id);
 		
-		producers = uag.data.Producers.get();
+		producers = uag.field.dataContext.Producers.get();
 		
 		expect(producers).not.toEqual(null);
 		expect(producers).not.toEqual([]);
@@ -218,11 +218,11 @@ describe("Import", function() {
 		
 		// add the same producer to producers list (for the second time) 
 		expect(function(){
-			uag.data.Import.addProducer(_jsonProducer1Data);
+			uag.field.dataContext.Import.addProducer(_jsonProducer1Data);
 		}).toThrow("Producer already imported");
 		
 		// get producers 
-		producers = uag.data.Producers.get();
+		producers = uag.field.dataContext.Producers.get();
 		
 		expect(producers).not.toEqual(null);
 		expect(producers).not.toEqual([]);
@@ -230,7 +230,7 @@ describe("Import", function() {
 		expect(producers[0].id).toEqual(_producerId1);
 		
 		/*
-		var qualities = uag.data.Producers.getQualities();
+		var qualities = uag.field.dataContext.Producers.getQualities();
 		
 		expect(qualities[0].id).toEqual(_jsonProducer1Data.qualities[0].id);
 		expect(qualities[0].name).toEqual(_jsonProducer1Data.qualities[0].name);
@@ -239,14 +239,14 @@ describe("Import", function() {
 		var products = new Array();
 		for ( var int = 0 ; int < _jsonProducer1Data.products.length ; int++ ) {
 			products.push(
-					new uag.data.model.Product(_jsonProducer1Data.products[int]));
+					new uag.field.dataContext.model.Product(_jsonProducer1Data.products[int]));
 		}
 		producer.products = products;
 		
 		var fields = new Array();
 		for ( var int = 0 ; int < _jsonProducer1Data.fields.length ; int++ ) {
-			var field = new uag.data.model.Field(_jsonProducer1Data.fields[int]);
-			var position = new uag.data.model.Position(_jsonProducer1Data.fields[int].position);
+			var field = new uag.field.dataContext.model.Field(_jsonProducer1Data.fields[int]);
+			var position = new uag.field.dataContext.model.Position(_jsonProducer1Data.fields[int].position);
 			field.position = position;
 			
 			for (var i = 0 ; i < products.length ; i++) {
@@ -269,22 +269,22 @@ describe("Field app uses cases", function() {
 	
 	it("Should add a first worker and retrieve empty containers list", function() {
 		
-		uag.data.WorkerContainers.clear();
+		uag.field.dataContext.WorkerContainers.clear();
 		
 		// worker name (ui input)
 		var workerName = "workerName1";
 		
 		// set worker  
-		var workerId = uag.data.WorkerContainers.setWorker(workerName, _producerId1);
+		var workerId = uag.field.dataContext.WorkerContainers.setWorker(workerName, _producerId1);
 		
 		// fetch data 
-		var worker = uag.data.WorkerContainers.getWorker();
+		var worker = uag.field.dataContext.WorkerContainers.getWorker();
 		
 		expect(worker.isAvailable).toEqual(true);
 		expect(worker.id).toEqual(workerId);
 		
 		// get worker's containers list 
-		var containers = uag.data.WorkerContainers.getContainers();
+		var containers = uag.field.dataContext.WorkerContainers.getContainers();
 		
 		expect(containers).not.toEqual(null);
 		expect(containers.length).toEqual(0);
@@ -293,37 +293,37 @@ describe("Field app uses cases", function() {
 	
 	it("Should fetch imported data with api ", function() {
 		
-		var producers = uag.data.Producers.get();
+		var producers = uag.field.dataContext.Producers.get();
 		
 		expect(producers.length).toEqual(1);
 		
-		var producer = uag.data.Producers.find();
+		var producer = uag.field.dataContext.Producers.find();
 		
 		expect(producer.id).toEqual(producers[0].id);
 		
-		var fields = uag.data.Producers.getFields();
+		var fields = uag.field.dataContext.Producers.getFields();
 		
 		expect(fields).not.toEqual(null);
 		expect(fields).not.toEqual([]);
 		expect(fields.length).toEqual(producers[0].fields.length);
 		
-		var field1 = uag.data.Producers.findField(fields[0].id);
+		var field1 = uag.field.dataContext.Producers.findField(fields[0].id);
 		
 		expect(field1).not.toEqual(undefined || null);
 		expect(field1.id).toEqual(fields[0].id);
 		
-		var products = uag.data.Producers.getProducts();
+		var products = uag.field.dataContext.Producers.getProducts();
 		
 		expect(products).not.toEqual(undefined || null);
 		expect(products.length).toEqual(producers[0].products.length);
 		
-		var productsField = uag.data.Producers.getFieldProducts(field1.id);
+		var productsField = uag.field.dataContext.Producers.getFieldProducts(field1.id);
 		
 		expect(productsField).not.toEqual(undefined || null);
 		expect(productsField.length).toEqual(producers[0].fields[0].productsId.length);
 		expect(productsField[0].id).toEqual(producers[0].fields[0].productsId[0]);
 		
-		var productField = uag.data.Producers.findFieldProduct(field1.id,productsField[0].id);
+		var productField = uag.field.dataContext.Producers.findFieldProduct(field1.id,productsField[0].id);
 		
 		expect(productField).not.toEqual(undefined || null);
 		expect(productField.id).toEqual(productsField[0].id);
@@ -333,15 +333,15 @@ describe("Field app uses cases", function() {
 	it("Should add a container to worker's containers list", function() {
 		
 		// get qualities list 
-		var qualities = uag.data.Producers.getQualities();
+		var qualities = uag.field.dataContext.Producers.getQualities();
 		// default selected quality 
 		var selectedQuality = qualities[0]; 
 		// get fields list 
-		var fields = uag.data.Producers.getFields();
+		var fields = uag.field.dataContext.Producers.getFields();
 		// default selected field 
 		var selectedField = fields[0]; 
 		// get field products list (depend on selected field)
-		var products = uag.data.Producers.getFieldProducts(selectedField.id);
+		var products = uag.field.dataContext.Producers.getFieldProducts(selectedField.id);
 		// default selected product
 		var selectedProduct = products[0]; 
 		
@@ -350,7 +350,7 @@ describe("Field app uses cases", function() {
 		// user select a field (user select list input)
 		selectedField = fields[1];
 		// get field products list (depend on selected field)
-		products = uag.data.Producers.getFieldProducts(selectedField.id);
+		products = uag.field.dataContext.Producers.getFieldProducts(selectedField.id);
 		// select a product (user select list input)
 		selectedProduct = products[1]; 
 		
@@ -362,27 +362,27 @@ describe("Field app uses cases", function() {
 		
 		// add container to worker's containers list 
 		var containerId = 
-			uag.data.WorkerContainers.addContainer(
+			uag.field.dataContext.WorkerContainers.addContainer(
 					weight,
 					selectedQuality, 
 					selectedField, 
 					selectedProduct);
 		
-		var containers = uag.data.WorkerContainers.getContainers();
+		var containers = uag.field.dataContext.WorkerContainers.getContainers();
 		
 		expect(containers).not.toEqual(undefined || null);
 		expect(containers).not.toEqual([]);
 		expect(containers.length).toEqual(1);
 		
-		var container = uag.data.WorkerContainers.findContainer(containerId);
+		var container = uag.field.dataContext.WorkerContainers.findContainer(containerId);
 		
 		expect(container).not.toEqual(undefined || null);
 		expect(container).not.toEqual({});
 		expect(container.id).toEqual(containerId);
 		
-		uag.data.WorkerContainers.removeContainer(containerId);
+		uag.field.dataContext.WorkerContainers.removeContainer(containerId);
 				
-		containers = uag.data.WorkerContainers.getContainers();
+		containers = uag.field.dataContext.WorkerContainers.getContainers();
 		
 		expect(containers).not.toEqual(undefined || null);
 		expect(containers).toEqual([]);
